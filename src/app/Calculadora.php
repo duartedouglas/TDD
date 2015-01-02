bd583f58b2bb86cb1a55a3173842d177f2da4ad4
Douglas Duarte <douglasg3x@hotmail.com>
2015-01-02 16:16:01 -0200

    adciona novo teste

--- src/app/Calculadora.php
+++ src/app/Calculadora.php
@@ -6,6 +6,10 @@ class Calculadora
 {
 	public function soma($a, $b)
 	{
+		if ( ! is_int($a) or ! is_int($b) ) {
+			throw new \InvalidArgumentException("Error Processing Request", 1);
+			
+		}
 		return $a + $b;
 	}
 
--- tests/App/CalculadoraTest.php
+++ tests/App/CalculadoraTest.php
@@ -1,13 +1,25 @@
 <?php
- namespace App;
+ namespace AppTest;
  
+ use App\Calculadora;
+
 class CalculadoraTest extends \PHPUnit_Framework_TestCase
 {
 	
-	function testSoma()
+	public function testSoma()
 	{
 		$calc = new Calculadora();
 
 		$this->assertEquals(4, $calc->soma(2, 2));
 	}
+	/**
+	* @expectedException InvalidArgumentException
+	*/
+    public function testSomaThrowsInvalidArgumentException()
+    {
+    	$calc = new Calculadora();
+
+	    $calc->soma('2', []);
+    }
+
 }
